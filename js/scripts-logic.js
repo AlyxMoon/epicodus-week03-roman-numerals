ff9c00225ec34077c79153cd903fcef15bc514c8
AlyxMoon <allisterkmoon@gmail.com>
2021-06-10 10:03:21 -0700

    write code to pass the first round of tests

--- js/scripts-logic.js
+++ js/scripts-logic.js
@@ -1,4 +1,11 @@
 
 
 function convertToRomanNumerals (number) {
+  if (number === 1) return 'I'
+  if (number === 5) return 'V'
+  if (number === 10) return 'X'
+  if (number === 50) return 'L'
+  if (number === 100) return 'C'
+  if (number === 500) return 'D'
+  if (number === 1000) return 'M'
 }
